2d198f05d0d8a25ce17d153b5f36dc64956fb673
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-01-22 12:56:30 -0500

    working on local storage

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,7 +1,7 @@
-// START TIMER
-// Create var with function of get moment and 
-var data = [];
 
+var data = JSON.parse(localStorage.getItem('array')) || [];
+
+// clock
 var updateInfo = function () {
     date = moment();
     datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
@@ -13,13 +13,15 @@ $(document).ready(function(){
     setInterval(updateInfo, 1000);
     updateBackground();
 });
-// Creat for loop but create var 
+// var for desription box text area color past, present, future
+// background color changes as the time advances throught the day 
 var updateBackground = function () {
-    // var it need be above so that we call the spefic hour
-    let hour = date.format('H');
+    // call the specific hour
+    let hour = parseInt(date.format('H'));
    //TEST hour -= 7
     for (i = 9; i < 18; i++) {
     var textArea = $("#" + i); 
+// classes for past, present, future come from CSS
 // condition 1
   if (i < hour) {
       textArea.addClass("past") 
@@ -33,22 +35,48 @@ var updateBackground = function () {
       textArea.addClass("future")
 
   }
-  console.log(hour)
+  // console.log(hour) //
 }
 }
 
 
 
-  $("button").on('click', function(){ 
-    let btn = $(this)
-    let hour = btn.data('hour')
+$("button").on('click', function(event){ 
+  event.preventDefault();
+  
+  let btn = $(this)
+  let hour = btn.data('hour')
+  //console.log ($(this).data('hour'))
+  let content = $("#" + hour).val()
 
-// get content from the text area
+// is content for the specified content/hour
+    var duplicate = false
+// check contnet   
+for (let i= 0; i < data.length; i++) {
+  const element = data[i];
+// seting the content setting equal 
+ if (element['hour'] == hour) {
+      element['content'] = content; //update
+      duplicate = true; 
+  // console.log("duplicate", "found")
+  } 
+} 
+if ( !duplicate ){ 
+  var obt = {'hour': hour, 'content': content } //  console.log(hour, content)
+   data.push(obt) 
+}  //console.log (data)  
+// // hi, has the hour loop. through array update it. look through it find the hour we 
+// // up date the spefic data.  
 
-// git content of text area 
-// use content to update data array
-// hi, has the hour loop. through array update it. look through it find the hour we 
-// up date the spefic data.  
-    localStorage.setItem('hour', JSON.stringify(data));
-  console.log (hour)
-  })
+
+localStorage.setItem('array', JSON.stringify(data));
+
+alert (localStorage.getItem('array'));
+
+
+
+})  // git content from the text area
+
+// local storage .get itmen 
+// call array data
+// .ready
