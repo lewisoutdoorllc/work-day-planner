193ca778d4d776602047a80ba2ad987813d172d2
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-01-23 12:36:52 -0500

    fixed local storage issues

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,5 @@
-
-var data = [];
+// Global variable to hold task data
+var taskData = [];
 
 // clock
 var updateInfo = function () {
@@ -7,35 +7,53 @@ var updateInfo = function () {
     datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
 };
 
+var getAndUpdateData = function () {
+  // Check for info local storage
+  if (localStorage.getItem('taskData')) {
+    taskData = JSON.parse(localStorage.getItem('taskData'));
+
+    for (let i= 0; i < taskData.length; i++) {
+      for (j= 9; j < 18; j++) {
+        if (taskData [i].hour == j) {
+          $("#" + j) .val(taskData [i].content)
+        }
+      }
+    }
+
+  }
+}
+
 $(document).ready(function(){
     datetime = $('#currentDay')
     updateInfo();
     setInterval(updateInfo, 1000);
     updateBackground();
+    getAndUpdateData();
 });
+
 // var for desription box text area color past, present, future
 // background color changes as the time advances throught the day 
 var updateBackground = function () {
-    // call the specific hour
-    let hour = parseInt(date.format('H'));
-   //TEST hour -= 7
-    for (i = 9; i < 18; i++) {
+  // call the specific hour
+  let hour = parseInt(date.format('H'));
+  //TEST hour -= 7
+  for (i = 9; i < 18; i++) {
     var textArea = $("#" + i); 
-// classes for past, present, future come from CSS
-// condition 1
-  if (i < hour) {
-      textArea.addClass("past") 
-  }
-// conditon 2 
-  else if (i === hour) {
-      textArea.addClass("present")
-  }
-// condition 3
-  else {
-      textArea.addClass("future")
+  // classes for past, present, future come from CSS
+  // condition 1
+    if (i < hour) {
+        textArea.addClass("past") 
+    }
+  // conditon 2 
+    else if (i === hour) {
+        textArea.addClass("present")
+    }
+  // condition 3
+    else {
+        textArea.addClass("future")
 
+    }
   }
-}
 }
 
 /*
@@ -43,42 +61,32 @@ function renderDescription(data) {
   $('array').empty();
 */
 
-$("button").on('click', function(event){ 
+$(document).on('click', "button", function(event){ 
   event.preventDefault();
   let btn = $(this)
   let hour = btn.data('hour')
-  //console.log ($(this).data('hour'))
   let content = $("#" + hour).val()
 
-  //renderDescription(data);
-    var duplicate = false
-// check contnet   
-for (let i= 0; i < data.length; i++) {
-  const element = data[i];
-// seting the content setting equal 
- if (element['hour'] == hour) {
-      element['content'] = content; //update
-      duplicate = true; 
-  // console.log("duplicate", "found")
+    var exists = false
+  // loop through array  
+  for (let i= 0; i < taskData.length; i++) {
+    const element = taskData[i];
+    //  check for an existing record 
+    if (element['hour'] == hour) {
+        //Update the content of the existing record
+        element['content'] = content; //update
+        exists = true;
+        console.log("record exists")
+        // end the for loop
+        break; 
+    } 
   } 
-} 
-if ( !duplicate ){ 
-  var obt = {'hour': hour, 'content': content } //  console.log(hour, content)
-   data.push(obt) 
-}  console.log (data)  
+  if ( !exists ){ 
+    var obt = {'hour': hour, 'content': content } //  console.log(hour, content)
+    taskData.push(obt) 
+  }  
+  console.log (taskData)  
 
-//renderDescription(data);
+  localStorage.setItem('taskData', JSON.stringify(taskData));
+});
 
-localStorage.setItem('array', JSON.stringify(data));
-})
-
-var array = JSON.parse(localStorage.getItem('array'));
-if (array) {
-for (let i= 0; i < array.length; i++) {
-for (j= 9; j < 18; j++) {
-  if (array [i].hour == j) {
-    $("#" + j) .val(array [i].content)
-  }
-}
-}
-}
